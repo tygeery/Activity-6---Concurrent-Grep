c8227c6125842061f05f4a7fbbd05d28ceed146b
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-10-31 16:52:12 -0400

    Some basic details added

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,10 +1,17 @@
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by Tyler on 10/28/14.
+ * Main Driver
  */
 public class CGrep {
 
 
     public static void main(String [ ] args) {
+        final ExecutorService pool = Executors.newFixedThreadPool(3);
+        final ExecutorCompletionService completionService = new ExecutorCompletionService(pool);
 
     }
 
---
+++ src/Found.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * File created by jcdesimp on 10/31/14.
+ */
+public class Found {
+    private String fileName;
+    private List<String> matchingLines;
+
+
+}
